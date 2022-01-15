498b9b6dee4384c76bc938fb53fe4d12b36cc2e9
James Pritchard <james.edward.pritchard@gmail.com>
2022-01-14 20:26:14 -0500

    3. delegated transfer

--- contracts/KingOfTheForestCoin.sol
+++ contracts/KingOfTheForestCoin.sol
@@ -7,19 +7,30 @@ contract KingOfTheForestCoin{
     string public standard = "KOTF Coin v1.0";
     uint256 public totalSupply;
 
-    mapping(address => uint256) public balanceOf;
 
     event Transfer(
         address indexed _from,
         address indexed _to,
         uint256 _value
     );
+    
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address=> mapping(address=>uint256)) public allowance;
 
     constructor(uint256 _initialSupply){
         balanceOf[msg.sender]= _initialSupply;
         totalSupply = _initialSupply;
         }
+
+
         //transfer
         function transfer(address _to, uint256 _value) public returns(bool success){
         //transfer exception is account doesnt have enough 
@@ -34,5 +45,29 @@ contract KingOfTheForestCoin{
 
 
         }
-    
+    //approve
+    function approve(address _spender, uint256 _value) public returns(bool success){
+        //allowance
+        allowance[msg.sender][_spender]= _value;
+
+        //Approve event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+    //transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender]-= _value;
+
+
+        emit Transfer(_from, _to, _value);
+        
+        return true;
+    }
 }
