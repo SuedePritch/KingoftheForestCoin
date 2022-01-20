ca99d31463f718c91221b0bfdff7ec1a869beebb
James Pritchard <james.edward.pritchard@gmail.com>
2022-01-20 15:06:47 -0500

    Compiles files for DIST

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // ...
+    entry: {
+      "app": "src/app.js"
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: "[name].js",
+      sourceMapFilename: "[name].js.map"
+    },
+    devtool: "source-map"
+    // ...
+};
