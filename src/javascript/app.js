3d0a22c18016641cf197af2d43141c63de6537f9
James Pritchard <james.edward.pritchard@gmail.com>
2022-01-21 02:16:45 -0500

    frontend update

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -50,6 +50,28 @@ App = {
             console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
         }
     },
+
+//     loadWeb3: async () => { 
+//         await detectEthereumProvider()
+
+//         if (provider) {
+
+//         console.log('Ethereum successfully detected!')
+
+//   // From now on, this should always be true:
+//   // provider === window.ethereum
+
+//   // Access the decentralized web!
+
+//   // Legacy providers may only have ethereum.sendAsync
+//     const chainId = await provider.request({
+//     method: 'eth_chainId'
+//     })
+// } else {
+
+//   // if the provider is not detected, detectEthereumProvider resolves to null
+//     console.error('Please install MetaMask!', error)
+// }},
     
     loadAccount: async () =>{
         App.account = web3.eth.accounts[0]
